8cf02e0da35fd1a863d6b01f8c48c3571a074c14
Dylan-Walters-Student <112421363+Dylan-Walters-Student@users.noreply.github.com>
2023-03-29 12:52:18 -0400

    Added various coins

--- Assets/Scripts/CoinPickup.cs
+++ Assets/Scripts/CoinPickup.cs
@@ -7,15 +7,27 @@ public class CoinPickup : MonoBehaviour
     [SerializeField] AudioClip coinPickupSFX;
     int coinSmall = 1;
     int coinMedium = 5;
-    int coinlarge = 10;
+    int coinLarge = 10;
 
     bool wasCollected = false;
-    void OnTriggerEnter2D(Collider2D other) 
+    void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Player" && !wasCollected)
         {
             wasCollected = true;
-            FindObjectOfType<GameSession>().AddToScore(coinMedium);
+            if (gameObject.tag == "CoinSmall")
+            {
+                FindObjectOfType<GameSession>().AddToScore(coinSmall);
+            }
+            else if (gameObject.tag == "CoinMedium")
+            {
+                FindObjectOfType<GameSession>().AddToScore(coinMedium);
+            }
+            else 
+            {
+                FindObjectOfType<GameSession>().AddToScore(coinLarge);
+            }
+            
             AudioSource.PlayClipAtPoint(coinPickupSFX, Camera.main.transform.position);
             Destroy(gameObject);
         }
