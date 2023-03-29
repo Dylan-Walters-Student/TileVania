d7cb2fa2713bc5adb1b873069b470f64dec099d2
Dylan-Walters-Student <112421363+Dylan-Walters-Student@users.noreply.github.com>
2023-03-29 14:47:20 -0400

    FINISHED!!

--- Assets/Scripts/LevelExit.cs
+++ Assets/Scripts/LevelExit.cs
@@ -13,8 +13,6 @@ public class LevelExit : MonoBehaviour
         {
             StartCoroutine(LoadNextLevel());
         }
-        
-
     }
 
     IEnumerator LoadNextLevel()
@@ -28,7 +26,7 @@ public class LevelExit : MonoBehaviour
         {
             nextSceneIndex = 0;
         }
-
+        FindObjectOfType<ScenePersist>().ResetScenePersist();
         SceneManager.LoadScene(nextSceneIndex);
     }
 }
